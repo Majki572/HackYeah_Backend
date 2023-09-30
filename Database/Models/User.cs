598d76a9b33bdf6df210c21630b4b7253c1117f3
Nina <nina.labecka@gmail.com>
2023-09-30 16:12:55 +0200

    Fix user class

--- Database/Models/User.cs
+++ Database/Models/User.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Database.Models.Fridge;
 
 namespace Database.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Username { get;set; }
+    public string Username { get; set; }
     public string? Password { get; set; }
     public string Location { get; set; }
     public string? Email { get; set; }
-
-
-        public Fridge.Fridge Fridge { get; set; }
-    }
+    public Fridge Fridge { get; set; }
+}
 }
