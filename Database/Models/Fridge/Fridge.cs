2f999eb13b132ce2031a700f6eae1528a1bf1640
Majkel572 <mrefe12345@gmail.com>
2023-09-30 15:13:20 +0200

    database and models update

---
+++ Database/Models/Fridge/Fridge.cs
@@ -0,0 +1,7 @@
+﻿namespace Database.Models.Fridge;
+public class Fridge {
+    public int Id { get; set; } 
+    public string Name { get; set; }
+    public List<Product> ProductList { get; set; }
+}
+
---
+++ Database/Models/Fridge/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Database.Models.Fridge;
+public class Product {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ExpirationDate { get; set; }
+    public string Description { get; set; }
+    public double? Weight { get; set; }
+    public double? Calories { get; set; }
+}
