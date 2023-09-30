abfcd71f3da28629cb30db0006ed28f3b5dc7010
Nina <nina.labecka@gmail.com>
2023-09-30 22:56:25 +0200

    quick fix

--- Backend/Controllers/GiveawaysController.cs
+++ Backend/Controllers/GiveawaysController.cs
@@ -110,7 +110,7 @@ namespace Backend.Controllers
         // POST: api/Giveaways
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Giveaway>> PostGiveaway([FromQuery]NewGiveawayDTO giveaway)
+        public async Task<ActionResult<Giveaway>> PostGiveaway([FromBody]NewGiveawayDTO giveaway)
         {
             var result = await giveawayService.CreateGiveaway((Giveaway)giveaway);
 
